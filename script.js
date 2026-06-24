88d2e12ab24c208a1a946d7616cf3862bab05334
poojithaai <poojithaaisvpcet@gmail.com>
2026-06-24 00:30:02 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("btn").addEventListener("click", function() {
+    document.getElementById("message").innerHTML =
+        "I am a passionate CSE student skilled in Python and C, always eager to learn new technologies.";
+});
